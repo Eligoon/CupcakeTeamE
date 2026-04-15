1f73bff30eb9176056377d69040a0c2950305a00
Eligoon <derpepind117@live.dk>
2026-04-15 14:48:29 +0200

    account and hashing corrected

--- src/main/java/dk/services/UserService.java
+++ src/main/java/dk/services/UserService.java
@@ -2,16 +2,28 @@ package dk.services;
 
 import dk.entities.User;
 import dk.exceptions.DatabaseException;
+import dk.persistence.AccountMapper;
 import dk.persistence.ConnectionPool;
 import dk.persistence.UserMapper;
+import dk.utils.PasswordHasher;
 
 public class UserService {
 
     public static void registerUser(String email, String password, String role, ConnectionPool cp)
             throws DatabaseException {
 
-        User user = new User(0, email, password, role);
+        // 1. hash password
+        String hashedPassword = PasswordHasher.hash(password);
+
+        // 2. create user
+        User user = new User(0, email, hashedPassword, role);
         UserMapper.createUser(user, cp);
+
+        // 3. fetch created user (to get ID)
+        User createdUser = UserMapper.getUserByEmail(email, cp);
+
+        // 4. create empty account
+        AccountMapper.createAccount(createdUser.getUserId(), 0,  cp);
     }
 
     public static User getUserByEmail(String email, ConnectionPool cp)
