6f972187383276d6b8525dd9a77f8ce6b301c905
LoveBytes25 <keramsby@gmail.com>
2026-04-16 04:09:50 +0200

    Added name, lines and total fields as well as getters and setters

--- src/main/java/dk/entities/Order.java
+++ src/main/java/dk/entities/Order.java
@@ -1,6 +1,7 @@
 package dk.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Order {
     private String customerName;
@@ -9,16 +10,32 @@ public class Order {
     private int userId;
     private LocalDateTime createdAt;
     private String status;
+    private List<CupcakeOrders> lines;
 
-    public Order(int orderId, int userId, LocalDateTime createdAt, String status) {
+    public Order(String customerName, double total, int orderId, int userId, LocalDateTime createdAt, String status, List<CupcakeOrders> lines) {
+        this.customerName = customerName;
+        this.total = total;
         this.orderId = orderId;
         this.userId = userId;
         this.createdAt = createdAt;
         this.status = status;
+        this.lines = lines;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getTotal() {
+        return total;
     }
 
     public int getOrderId() {
         return orderId;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public LocalDateTime getCreatedAt() {
@@ -27,5 +44,17 @@ public class Order {
 
     public String getStatus() {
         return status;
+    }
+
+    public List<CupcakeOrders> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<CupcakeOrders> lines) {
+        this.lines = lines;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
     }
 }
