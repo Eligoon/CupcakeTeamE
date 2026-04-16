3d2bba2f91cafe2e20f93664873a5afa4f747b52
Eligoon <derpepind117@live.dk>
2026-04-16 17:30:39 +0200

    arguements removed

--- src/main/java/dk/entities/Order.java
+++ src/main/java/dk/entities/Order.java
@@ -4,30 +4,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
-    private String customerName;
-    private double total;
     private int orderId;
     private int userId;
     private LocalDateTime createdAt;
     private String status;
-    private List<CupcakeOrders> lines;
 
-    public Order(String customerName, double total, int orderId, int userId, LocalDateTime createdAt, String status, List<CupcakeOrders> lines) {
-        this.customerName = customerName;
-        this.total = total;
+    public Order(int orderId, int userId, LocalDateTime createdAt, String status) {
         this.orderId = orderId;
         this.userId = userId;
         this.createdAt = createdAt;
         this.status = status;
-        this.lines = lines;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public double getTotal() {
-        return total;
     }
 
     public int getOrderId() {
@@ -44,17 +30,5 @@ public class Order {
 
     public String getStatus() {
         return status;
-    }
-
-    public List<CupcakeOrders> getLines() {
-        return lines;
-    }
-
-    public void setLines(List<CupcakeOrders> lines) {
-        this.lines = lines;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
     }
 }
